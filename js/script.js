e06f3cd6dead9ad4356dc2b27b99e7715e5088f1
lucacappello79 <postt79@gmail.com>
2023-03-23 09:34:22 +0100

    feat: ho dimenticato di pusharlo ieri, added bonus

--- js/script.js
+++ js/script.js
@@ -7,24 +7,29 @@ createApp({
         return {
 
             email: [],
+            counter: 0,
+            maxEmailAllowed: 10,
 
         };
 
     },
 
-        created() {
+    created() {
 
-            for (let i = 0; i < 10; i++) {
+        for (let i = 0; i < this.maxEmailAllowed; i++) {
   
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res) => {
 
                 console.log(res.data.response);
                 console.log(res);
-
+                
                 this.email.push(res.data.response);
+                this.counter++;
+
             })
+
         }
 
-        },
+    },
 
 }).mount('#app')
